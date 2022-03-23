d05542acce0ea7a7bc2d1f4aa055192e0b50de70
jodh <jodh@itu.dk>
2022-03-23 09:50:39 +0100

    week 6 start

--- app/src/main/java/dk/itu/garbageapp/ItemsDB.java
+++ app/src/main/java/dk/itu/garbageapp/ItemsDB.java
@@ -1,16 +1,15 @@
 package dk.itu.garbageapp;
 
+import android.app.Activity;
 import android.content.Context;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Map;
-import java.util.Observable;
 import java.util.TreeMap;
 
-public class ItemsDB extends Observable {
-    private static ItemsDB sItemsDb;
+public class ItemsDB {
 
     private final TreeMap<String, Item> sorter;
 
@@ -18,21 +17,15 @@ public class ItemsDB extends Observable {
      * Initialize the ItemsDB singleton.
      * Ensures that an ItemsDB object exists.
      */
-    public static void initialize(Context context) {
-        if (sItemsDb == null) {
-            sItemsDb = new ItemsDB(context);
-        }
+
+    public ItemsDB get(){
+        return ItemsDB.this;
     }
 
-    public static ItemsDB get(){
-        if(sItemsDb == null) throw new IllegalStateException("ItemsDB must be initialized");
-        return sItemsDb;
-    }
-
-    private ItemsDB(Context context) {
+    public ItemsDB() {
         sorter = new TreeMap<>();
         new GarbageCategories();
-        populateSorter(context);
+        populateSorter(/**There needs to be a context here and I haven't figured out how to fix that*/);
     }
 
     /**
@@ -42,32 +35,24 @@ public class ItemsDB extends Observable {
      * @return The registered category for a given input. Returns "not found" if there is no match.
      */
     public String lookUp(String input) {
-        if (sItemsDb.sorter.get(input) == null) {
+        if (get() == null) {
             return (input + " should be placed in: not found");
         } else {
             System.out.println("searching");
 
-            return sItemsDb.sorter.get(input).toString();
+            return sorter.get(input).toString();
         }
     }
 
     public void addItem (String item, String category) {
         sorter.put(item, new Item(item, category));
-        this.setChanged(); notifyObservers();// mark as changed and notify observers of change
-    }
-
-    public String listAll() {
-        StringBuilder result = new StringBuilder();
-        for (Map.Entry <String, Item> item: sorter.entrySet()) {
-            result.append("\n").append(item.getValue().toString());
-        }
-        return result.toString();
     }
 
     // setup method
 
     private void populateSorter(Context context){
 
+        // ok this isn't working if I want it to be working within the live data model
         // try-catch block largely adapted from https://github.itu.dk/jst/MMAD2022
         try {
             BufferedReader reader= new BufferedReader(
@@ -78,7 +63,6 @@ public class ItemsDB extends Observable {
                 sorter.put(gItem[0], new Item(gItem[0], gItem[1]));
                 line= reader.readLine();
             }
-            this.setChanged(); notifyObservers();
         } catch (IOException e) {  // Error occurred when opening raw file for reading.
         }
     }
